195b5ca61708c75a4821adfaa5e0a42b72f56bbc
Ben Hood <benhood739@gmail.com>
2023-01-07 18:53:53 +0000

    added winning losing replaying

--- js/game.js
+++ js/game.js
@@ -15,7 +15,14 @@ function rollDice(){
     document.getElementById("dice1").innerHTML = diceOne;
     document.getElementById("dice2").innerHTML = diceTwo;
 
-    setDiceOptions("inline");
+    if(checkLossPotential()){
+        document.getElementById("winLoss").innerHTML = "You Lost!";
+        endGame();
+    }
+    else if(checkWinPotential()){
+        document.getElementById("winLoss").innerHTML = "You Won!";
+        endGame();
+    }
 }
 
 function useTotal(){
@@ -37,4 +44,66 @@ function updateNumberDisplay(){
     }
 }
 
-setDiceOptions("none")
+function clearNumberDisplay(){
+    for(let i=1;i<=9;i++){
+        document.getElementById(i+"").setAttribute("style","color:#000000;");
+    }
+}
+
+function checkLossPotential(){
+    let total = diceOne + diceTwo;
+    let totalImpossible = false;
+    let splitImpossible = false;
+
+    if(shutNumbers.indexOf(total)!=-1 || total>9){
+        totalImpossible = true;
+    }
+    else{
+        document.getElementById("useTotal").style.display = "inline";
+        
+    }
+
+    if(shutNumbers.indexOf(diceOne)!=-1||shutNumbers.indexOf(diceTwo)!=-1 || diceOne == diceTwo){
+        splitImpossible = true;
+    }
+    else{
+        document.getElementById("useSplit").style.display = "inline";
+    }
+
+    if(totalImpossible&&splitImpossible){
+        return true;
+    }
+    return false;
+}
+
+function checkWinPotential(){
+    let won = true;
+    for(let i=1;i<=9;i++){
+        if(shutNumbers.indexOf(i) == -1){
+            won = false;
+        }
+    }
+    return won;
+}
+
+function endGame(){
+    document.getElementById("rollDice").style.display = "none";
+    setDiceOptions("none");
+
+
+    document.getElementById("playAgain").style.display = "inline";
+}
+
+function playAgain(){
+    document.getElementById("playAgain").style.display = "none";
+    document.getElementById("rollDice").style.display = "inline";
+    diceOne = 0;
+    diceTwo = 0;
+    document.getElementById("dice1").innerHTML = diceOne;
+    document.getElementById("dice2").innerHTML = diceTwo;
+    shutNumbers = new Array();
+    clearNumberDisplay();
+
+}
+
+setDiceOptions("none");
