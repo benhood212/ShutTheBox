8534e17353d08adceaa594c734eed7dafedf96cf
Ben Hood <benhood739@gmail.com>
2023-01-07 17:54:00 +0000

    added showing and hiding of buttons

--- js/game.js
+++ js/game.js
@@ -1,10 +1,21 @@
 let diceOne = 0;
 let diceTwo = 0;
 
+let shutNumbers = new Array();
+
+function setDiceOptions(style){
+    document.getElementById("useTotal").style.display = style;
+    document.getElementById("useSplit").style.display = style;
+}
+
 function rollDice(){
     diceOne = Math.floor(Math.random()*6) + 1;
     diceTwo = Math.floor(Math.random()*6) + 1;
 
     document.getElementById("dice1").innerHTML = diceOne;
     document.getElementById("dice2").innerHTML = diceTwo;
+
+    setDiceOptions("inline");
 }
+
+setDiceOptions("none")
